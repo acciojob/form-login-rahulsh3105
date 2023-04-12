561d0a8a32071c97d43d843e54781141e73f947c
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-12 11:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+var firstN=document.getElementById("fname").value;
+	var lastN=document.getElementById("lname").value;
+	alert(firstN+" "+lastN);
 }
